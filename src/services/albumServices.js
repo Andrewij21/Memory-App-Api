a10cc1663c5e574a716b58168559b46a128992c7
Andrewij21 <andrewijaya988@gmail.com>
2023-08-27 17:06:20 +0700

    feat: add logger and delete function

--- src/services/albumServices.js
+++ src/services/albumServices.js
@@ -1,27 +1,42 @@
 const Album = require("../models/albumModel.js");
 const isValidId = require("../utils/isValidId.js");
 const { requestResponse } = require("../utils/requestResponse.js");
+const getLogger = require("../utils/logger.js");
+const logger = getLogger(__filename);
 
 class AlbumServices {
   async get() {
     const album = await Album.find({});
+    logger.info(`Get ${album.length} photos `);
     return { ...requestResponse.success, data: album };
   }
   async create(body) {
     const result = await Album.create(body);
+    logger.info(`Create photo with ID ${result._id}  `);
     return { ...requestResponse.created, data: result };
   }
-  async update(body, id) {
-    if (!isValidId(id))
-      throw { ...requestResponse.bad_request, message: "Invalid id" };
+  async update(body, _id) {
+    if (!isValidId(_id))
+      throw { ...requestResponse.bad_request, message: "Invalid ID" };
     const photo = await Album.findOneAndUpdate(
-      { _id: id },
+      { _id },
       { ...body },
       { new: true }
     );
 
     if (!photo) throw { ...requestResponse.not_found };
 
+    logger.info(`Update photos with ID ${photo._id} `);
+    return { ...requestResponse.success, data: photo };
+  }
+  async delete(_id) {
+    if (!isValidId(_id))
+      throw { ...requestResponse.bad_request, message: "Invalid ID" };
+    const photo = await Album.findOneAndDelete({ _id });
+
+    if (!photo) throw { ...requestResponse.not_found };
+
+    logger.info(`Delete photo with ID ${photo._id} `);
     return { ...requestResponse.success, data: photo };
   }
 }
