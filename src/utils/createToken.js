a1ae36c562a3b91e2b9cec51f04d9929e2df0414
Andrewij21 <andrewijaya988@gmail.com>
2023-08-30 15:06:50 +0700

    feat: add create token helpers

---
+++ src/utils/createToken.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const getLogger = require("../utils/logger");
+const logger = getLogger(__filename);
+
+const createToken = async (payload) => {
+  try {
+    const accessToken = jwt.sign(payload, process.env.SECRET_ACCESS_TOKEN, {
+      expiresIn: "10s",
+    });
+
+    const refreshToken = jwt.sign(
+      { username: payload.email },
+      process.env.SECRET_REFRESH_TOKEN,
+      { expiresIn: "1d" }
+    );
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    logger.error(error.toString());
+  }
+};
+
+module.exports = createToken;
