df334ac4eddc93ee7ebedfe406c5ef4c9efa4810
Andrewij21 <andrewijaya988@gmail.com>
2023-09-05 06:31:33 +0700

    feat: add middleware verify jwt and roles

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,20 @@ const albumRoutes = require("./albumRoutes");
 const userRoutes = require("./userRoutes");
 const authRoutes = require("./authRoutes");
 const refreshTokenRoutes = require("./refreshTokenRoutes");
+const verifyJwt = require("../middlewares/verifyJwt");
+const verifyRoles = require("../middlewares/verifyRoles");
+const ROLES = require("../config/rolesLists");
+
 router.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
 router.use("/auth", authRoutes);
-router.use("/album", albumRoutes);
-router.use("/user", userRoutes);
 router.use("/refresh", refreshTokenRoutes);
+
+router.use(verifyJwt);
+router.use("/album", verifyRoles(ROLES.User, ROLES.Admin), albumRoutes);
+router.use("/user", verifyRoles(ROLES.Admin), userRoutes);
+// router.use("/user", userRoutes);
 
 module.exports = router;
