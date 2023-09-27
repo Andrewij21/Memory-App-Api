34efc6c0190c1d48421f0b4c0811f1473fd0ff05
Andrewij21 <andrewijaya988@gmail.com>
2023-09-27 14:23:38 +0700

    refactor: add some allow origin

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://127.0.0.1:5173", "http://localhost:3000"];
+const allowedOrigins = ["http://127.0.0.1:5174", "http://localhost:3000"];
 
 module.exports = allowedOrigins;
