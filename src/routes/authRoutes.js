f92a8b656824cde7534530252888621904f3e9f2
Andrewij21 <andrewijaya988@gmail.com>
2023-09-27 14:23:58 +0700

    feat: add forget password route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,5 +3,6 @@ const authController = require("../controllers/authControllers");
 
 router.route("/").post(authController.login);
 router.route("/logout").delete(authController.logout);
+router.route("/forget-password").delete(authController.logout);
 
 module.exports = router;
