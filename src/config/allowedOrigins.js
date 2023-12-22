6562a414475773726bf5d38ea4e0b927ffbc1f8b
andrewijaya <andrewijaya988@gmail.com>
2023-12-22 11:14:06 +0700

    update: add allowed origins

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["http://127.0.0.1:5174", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://127.0.0.1:5174",
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://memory-phi-ten.vercel.app",
+];
 
 module.exports = allowedOrigins;
